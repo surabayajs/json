dbca42c07a2b245c544cba62fb037401f3d32867
Griko Nibras <git@griko.id>
2022-03-15 06:00:14 +0700

    feat: create build script listing json files

---
+++ build-index.mjs
@@ -0,0 +1,44 @@
+// @ts-check
+
+import * as fs from "fs/promises";
+import * as path from "path";
+
+const EXCLUDE_FILENAMES = [
+  "package.json",
+  //
+];
+
+/** @param {string[]} list */
+function makeTemplate(list = []) {
+  return `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>json.surabayajs.org</title>
+  </head>
+  <body>
+    <ul>
+      ${list.map((name) => `<li><a href="/${name}">${name}</a></li>`).join("")}
+    </ul>
+  </body>
+</html>
+`;
+}
+
+async function buildIndex() {
+  const list = await fs
+    .readdir(process.cwd())
+    .then((list) => list.filter((name) => name.endsWith(".json")))
+    .then((list) =>
+      list.filter((name) => {
+        return EXCLUDE_FILENAMES.findIndex((match) => name == match) < 0;
+      })
+    );
+
+  const outputPath = path.resolve(process.cwd(), "index.html");
+  fs.writeFile(outputPath, makeTemplate(list), { encoding: "utf-8" });
+}
+
+buildIndex();
